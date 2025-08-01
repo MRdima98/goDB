72cd09cf326737ca50db49051e17537ad0c3e248
MRdima98 <dimafrunza69@gmail.com>
2025-08-01 10:26:43 +0200

    bTree node rethinking

--- bTree/bTree.go
+++ bTree/bTree.go
@@ -5,23 +5,19 @@ import (
 	"slices"
 )
 
-const nrKeys = 16
+const maxKeysPerNode = 16
 
 type bTree struct {
-	indexes       [nrKeys]int
+	indexes       []int
 	children      []*bTree
 	indexZeroUsed bool
 }
 
 func (r *bTree) insertIndex(index int) error {
-	for i, el := range r.indexes {
-		if el == 0 {
-			r.indexes[i] = index
-			arr := r.indexes[:]
-			slices.Sort(arr)
-			r.indexes = [16]int(arr)
-			return nil
-		}
+	if len(r.indexes) < maxKeysPerNode {
+		r.indexes = append(r.indexes, index)
+		slices.Sort(r.indexes)
+		return nil
 	}
 
 	return errors.New("No space")
--- bTree/bTree_test.go
+++ bTree/bTree_test.go
@@ -1,6 +1,7 @@
 package btree
 
 import (
+	"math/rand"
 	"slices"
 	"testing"
 )
@@ -15,9 +16,7 @@ func TestInserOneIndex(t *testing.T) {
 }
 
 func TestNodePreservesOrder(t *testing.T) {
-	base := [16]int{3, 5, 7}
-	expected := base[:]
-	slices.Sort(expected)
+	expected := []int{3, 5, 7}
 	tree := &bTree{}
 	err := tree.insertIndex(3)
 	err = tree.insertIndex(7)
@@ -25,5 +24,25 @@ func TestNodePreservesOrder(t *testing.T) {
 
 	if err != nil || !slices.Equal(tree.indexes[:], expected) {
 		t.Errorf(`Expected order: %v\n got instead: %v`, expected, tree.indexes)
+	}
+}
+
+func TestOverflowShouldCreateNewNode(t *testing.T) {
+	var expected []int
+	expected = append(expected, 0)
+	tree := &bTree{}
+	tree.insertIndex(0)
+	tmp := 0
+	for range 15 {
+		tmp += rand.Intn(100)
+		expected = append(expected, tmp)
+		tree.insertIndex(tmp)
+	}
+	tree.insertIndex(1)
+	slices.Sort(expected)
+
+	if !slices.Equal(expected, tree.indexes[:]) {
+		t.Errorf(`Expected array: %v
+		 got instead: %v`, expected, tree.indexes)
 	}
 }
